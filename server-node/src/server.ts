624030f470077815f7fdaf7b6ca269dfb1e0d097
Igor Cardoso <igorgcardoso7@gmail.com>
2023-05-24 19:23:54 -0300

    🚧 Finish NLW

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -1,13 +1,32 @@
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import 'dotenv/config'
 import fastify from 'fastify'
+import { resolve } from 'node:path'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
